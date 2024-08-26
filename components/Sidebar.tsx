1980579482f4581f79b05271220dc2192c873bf3
Jonas Kwan <jonastheitguy@gmail.com>
2024-08-26 16:56:36 +0800

    Change the margin of the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Sidebar = () => {
   return (
     <aside className='h-screen z-20'>
-      <nav className='h-full flex flex-col bg-gradient-to-b from-gray-100 to-white border-r border-gray-300 shadow-lg w-[220px] overflow-auto'>
+      <nav className='h-full flex flex-col bg-gradient-to-b from-gray-100 to-white border-r border-gray-300 shadow-lg w-[220px] overflow-auto mt-0.5'>
         <div className='flex flex-col justify-between h-full px-6 py-4'>
           <div className='text-gray-700'>
             asdasd
