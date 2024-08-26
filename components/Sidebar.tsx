993bbfd6439da197dd5957d36c4b04fa5c9428a5
Jonas Kwan <jonastheitguy@gmail.com>
2024-08-26 16:32:10 +0800

    Modified sidebar size

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 
-const Siderbar = () => {
+const Sidebar = () => {
   return (
-   <aside className='h-screen'>
-    <nav className='h-full flex flex-col bg-white border-r shadow-sm'>
-      <div className='flex justify-between items-start p-4 '>
-asdasd
-asdasd
-      </div>
-
-    </nav>
-
-   </aside>
+    <aside className='h-screen z-20'>
+      <nav className='h-full flex flex-col bg-white border-r shadow-sm w-[210px]'>
+        <div className='flex justify-between items-start p-4'>
+          asdasd
+          asdasd
+        </div>
+      </nav>
+    </aside>
   )
 }
 
-export default Siderbar
+export default Sidebar
