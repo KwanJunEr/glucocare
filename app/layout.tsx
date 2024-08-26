d5e1173f971092268795ce3cb510bdd370afa4f9
Jonas Kwan <jonastheitguy@gmail.com>
2024-08-26 16:45:25 +0800

    fixed section and sidebar conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,12 +27,13 @@ export default function RootLayout({
           <Navbar/>
           <div className="flex">
                <Siderbar/>
-          </div>
-          <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+               <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
                 <div className='w-full '>
                 {children}
                 </div>
           </section>
+          </div>
+          
         </main>
   
         </body>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 const Sidebar = () => {
   return (
     <aside className='h-screen z-20'>
-      <nav className='h-full flex flex-col bg-gradient-to-b from-gray-100 to-white border-r border-gray-300 shadow-lg w-[220px]'>
-        <div className='flex justify-between items-start px-6 py-4'>
-          <div className='text-gray-700'>asdasd</div>
-          <div className='text-gray-700'>asdasd</div>
+      <nav className='h-full flex flex-col bg-gradient-to-b from-gray-100 to-white border-r border-gray-300 shadow-lg w-[220px] overflow-auto'>
+        <div className='flex flex-col justify-between h-full px-6 py-4'>
+          <div className='text-gray-700'>
+            asdasd
+          </div>
+          <div className='text-gray-700'>
+            asdasd
+          </div>
         </div>
       </nav>
     </aside>
