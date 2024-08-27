11faa90610d1938a7707a2d2ef9b2db9cc1b4d48
Jonas Kwan <jonastheitguy@gmail.com>
2024-08-27 16:41:30 +0800

    Completed 30 percent of the Home Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
-'use client';
+"use client";
 import { motion } from "framer-motion";
 
-import { TypewriterEffect, TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { Button, Card } from "antd";
+
+import {
+  TypewriterEffect,
+  TypewriterEffectSmooth,
+} from "@/components/ui/typewriter-effect";
 import Image from "next/image";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import ProfilePictureCard from "@/components/ProfilePictureCard";
 import ProfileInfo from "@/components/ProfileInfo";
 const words1 = `Empowering Your Journey to Better Diabetes Control.`;
- 
+
 const words = [
   {
     text: "Welcome,",
@@ -26,22 +31,62 @@ export default function Home() {
   return (
     <div>
       <div className="py-4 mb-4">
-        <TypewriterEffectSmooth words = {words}/>
-        <TextGenerateEffect words ={words1}/>
+        <TypewriterEffectSmooth words={words} />
+        <TextGenerateEffect words={words1} />
       </div>
       <div className="py-2 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
-        <ProfilePictureCard/>
-        <ProfileInfo/>
+        <ProfilePictureCard />
+        <ProfileInfo />
       </div>
       <div className="py-2 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
-        <div>
-          
-        </div>
-        <div>
+       
+      <Card 
+        hoverable
+        title = "Quick Actions"
+        className="md:w-[400px] w-full">
+          <div className="border-b  pb-2 shadow-sm flex flex-row items-center justify-between">
+            <div className="flex flex-col space-y-1">
+            <p className="">Eat Medicine</p>
+            <p>8.30a.m.</p>
+            </div>
+            <div>
+              <div className="rounded-full bg-green-200 text-center">
+                <span>Done</span>
+              </div>
+            </div>
 
-        </div>
+          </div>
+
+          <div className="border-b  py-2 shadow-sm flex flex-row items-center justify-between">
+            <div>
+            <p className="">Eat Medicine</p>
+            <p>8.30a.m.</p>
+            </div>
+            <div>
+              <div className="rounded-full bg-green-200 text-center">
+                <span>Done</span>
+              </div>
+            </div>
+
+          </div>
+
+      <p>Card content</p>
+      <p>Card content</p>
+         
+          
+        </Card>
+          <Card
+            hoverable
+            title="Daily Health Checking"
+            extra={<Button>Add Reminder</Button>}
+            className="md:w-[800px] w-full"
+          >
+           
+          </Card>
+
+      
+       
       </div>
     </div>
-
   );
 }
