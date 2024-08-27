82a8e142a2f6050bfc1f6a737f6df768a4c491c9
Jonas Kwan <jonastheitguy@gmail.com>
2024-08-27 15:44:16 +0800

    Updated Homepage
    Still working on the Sidebar Menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           <Navbar/>
           <div className="flex">
                <Siderbar/>
-               <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+               <section className='flex min-h-screen flex-1 flex-col p-8'>
                 <div className='w-full '>
                 {children}
                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
-import Image from "next/image";
+'use client';
+import { motion } from "framer-motion";
 
+import { TypewriterEffect, TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import Image from "next/image";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import ProfilePictureCard from "@/components/ProfilePictureCard";
+import ProfileInfo from "@/components/ProfileInfo";
+const words1 = `Empowering Your Journey to Better Diabetes Control.`;
+ 
+const words = [
+  {
+    text: "Welcome,",
+  },
+  {
+    text: "Kwan",
+  },
+  {
+    text: "Jun",
+  },
+  {
+    text: "Er",
+  },
+];
 export default function Home() {
   return (
-   
- <>
- </>
+    <div>
+      <div className="py-4 mb-4">
+        <TypewriterEffectSmooth words = {words}/>
+        <TextGenerateEffect words ={words1}/>
+      </div>
+      <div className="py-2 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
+        <ProfilePictureCard/>
+        <ProfileInfo/>
+      </div>
+      <div className="py-2 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
+        <div>
+          
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+
   );
 }
