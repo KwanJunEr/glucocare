8dd632ba2e4d8ec07364d6917014aba8f510eb52
Jonas Kwan <jonastheitguy@gmail.com>
2024-09-03 22:17:22 +0800

    Fixed the Layouts
    Fixed the layout to make it at least a bit more responsive and fixed the card titles

--- app/page.tsx
+++ app/page.tsx
@@ -31,63 +31,74 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="py-4 mb-4 flex flex-col md:flex-col items-start w-full space-x-5 justify-start">
+        {/*Text Parts*/}
         <div className="flex flex-col">
           <TypewriterEffectSmooth words={words} />
           <TextGenerateEffect words={words1} />
         </div>
+        {/*Text Parts*/}
       </div>
+
+      {/*Profile Carsd*/}
       <div className="py-2 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
         <ProfilePictureCard />
         <ProfileInfo />
       </div>
+      {/*Profile Carsd*/}
+
       <div className="py-2 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
-      <Card 
-        hoverable
-        title = "Quick Actions"
-        className="md:w-[400px] w-full !cursor-default">
-          <div className="border-b  pb-2 shadow-sm flex flex-row items-center justify-between">
+        <Card
+          hoverable
+          title="Quick Actions"
+          className="md:w-[400px] w-full !cursor-default"
+        >
+         
+        </Card>
+        <Card
+          hoverable
+          title="Daily Health CheckIns"
+          extra={<Button>Add Reminder</Button>}
+          className="md:w-[800px] w-full !cursor-default"
+        >
+
+<div className="border-b  pb-2 shadow-sm flex flex-row items-center justify-between">
             <div className="flex flex-col space-y-1">
-            <p className="">Eat Medicine</p>
-            <p>8.30a.m.</p>
+              <p className="">Eat Medicine</p>
+              <p>8.30a.m.</p>
             </div>
             <Badge color="red" text="Missed" />
           </div>
 
           <div className="border-b  py-2 shadow-sm flex flex-row items-center justify-between">
             <div>
-            <p className="">Eat Medicine</p>
-            <p>8.30a.m.</p>
+              <p className="">Eat Medicine</p>
+              <p>8.30a.m.</p>
             </div>
             <Badge color="green" text="Done" />
           </div>
 
-      <p>Card content</p>
-      <p>Card content</p>
-        </Card>
-          <Card
-            hoverable
-            title="Daily Health Checking"
-            extra={<Button>Add Reminder</Button>}
-            className="md:w-[800px] w-full !cursor-default"
-          >
-          </Card>
-      </div>
+          <p>Card content</p>
+          <p>Card content</p>
 
-      <div className="py-2 flex w-[1220px] flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
+
+        </Card>
+        </div>
+
+        <div className="py-2 flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5">
           <Card
             hoverable
-            title="Daily Health Checking"
+            title="Recent Health Data"
             extra={<Button>Add Reminder</Button>}
-            className="md:w-[800px] w-full !cursor-default"
-          >
-          </Card>
+            className="md:w-[600px] w-full !cursor-default"
+          ></Card>
+
           <Card
             hoverable
-            title="Daily Health Checking"
+            title="Reminders"
             extra={<Button>Add Reminder</Button>}
-            className="md:w-[800px] w-full !cursor-default"
-          >
-          </Card>
+            className="md:w-[600px] w-full !cursor-default"
+          ></Card>
+       
       </div>
     </div>
   );
