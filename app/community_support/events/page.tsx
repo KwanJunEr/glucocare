36150e6798012bf0ecb0bae7e1fdc3ce018c8395
KwanJunEr <jonastheitguy@gmail.com>
2024-09-09 15:22:01 +0800

    Completed Partially on Exercise, Finishing setting up all the files,  Completed partially on diets and nutrition page

---
+++ app/community_support/events/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+        <div>
+            <h1>Explore Events</h1>
+        </div>
+        <div className='grid grid-cols-4'>
+            <div className='col-span-3'>
+
+
+            </div>
+            <div className='col-span1'>
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Events
--- app/healthdashboard/page.tsx
+++ app/healthdashboard/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const HealthDashboard = () => {
   return (
     <div>
+      Wlecome 
       
     </div>
   )
