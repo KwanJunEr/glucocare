c31b72849ee6afb9c5afaa2b2e155d3f9a4916a2
KwanJunEr <jonastheitguy@gmail.com>
2024-09-23 04:53:00 +0800

    Complete the event page layout setup

--- app/community_support/events/page.tsx
+++ app/community_support/events/page.tsx
@@ -5,13 +5,37 @@ const Events = () => {
     <div>
         <div>
             <h1>Explore Events</h1>
+            <p>Ex</p>
         </div>
         <div className='grid grid-cols-4'>
             <div className='col-span-3'>
+              <div className='flex flex-row'>
 
+
+              </div>
+
+              <div className='flex flex-col'>
+                {/*Event Row 1 */}
+                <div className='flex flex-row'>
+
+                </div>
+
+                {/*Event Row 2 */}
+
+                <div className='flex flex-row'>
+
+                </div>
+              
+              </div>
 
             </div>
             <div className='col-span1'>
+
+              {/*Past Events */}
+              <div>
+
+                
+              </div>
 
             </div>
         </div>
