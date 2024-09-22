225097a9c46d78e3bf8c89ae57141d320be4d944
KwanJunEr <jonastheitguy@gmail.com>
2024-09-22 22:45:00 +0800

    Setup the health Dashboard Layout first

--- app/healthdashboard/page.tsx
+++ app/healthdashboard/page.tsx
@@ -2,8 +2,54 @@ import React from 'react'
 
 const HealthDashboard = () => {
   return (
-    <div>
-      Wlecome 
+    <div className='flex flex-col'>
+      {/*Introductory and Health Goal*/}
+      <div className='flex flex-row'>
+        <div className='w-[500px] rounded-md shadow-sm'>
+asdasd
+
+        </div>
+
+        {/*Health Goal*/}
+        <div>
+
+
+        </div>
+
+      </div>
+
+      {/*Health Cards */}
+      <div className='flex flex-row'>
+        <div>
+
+        </div>
+
+        <div>
+
+
+        </div>
+
+        <div>
+
+        </div>
+      </div>
+
+      {/*Graphs and Dashboard 6 Graphs */}
+      <div className='flex flex-col'>
+        {/* Three Graphs*/}
+        <div>
+
+        </div>
+
+        {/*Three Graphs */}
+
+        <div>
+
+          
+        </div>
+
+
+      </div>
       
     </div>
   )
