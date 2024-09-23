425573d39c24d6f4136a0a8f2a709363878fdff9
KwanJunEr <jonastheitguy@gmail.com>
2024-09-24 00:47:43 +0800

    Completed filling up the details for Events Page
    Fixed the Past Event Cards, Event Cards to accept props as well as adding relevant content

--- components/eventcards/PastEventCards.tsx
+++ components/eventcards/PastEventCards.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const PastEventCards = () => {
+interface PastEventCardsProps{
+  title : String;
+  description: String;
+}
+
+const PastEventCards = ({title, description}: PastEventCardsProps) => {
   return (
     <article
     className="rounded-lg border border-gray-100 bg-white p-4 shadow-sm transition hover:shadow-lg sm:p-6"
@@ -28,15 +33,12 @@ const PastEventCards = () => {
   
     <a href="#">
       <h3 className="mt-0.5 text-lg font-medium text-gray-900">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        {title}
       </h3>
     </a>
   
     <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur
-      animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem, mollitia
-      itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
-      Molestias explicabo corporis voluptatem?
+      {description}
     </p>
   
     <a href="#" className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600">
