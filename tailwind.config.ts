a869cef1ce7504b95a4b683857918e8c7d7b6756
KwanJunEr <jonastheitguy@gmail.com>
2024-09-24 08:59:59 +0800

    commit for merging

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 const svgToDataUri = require("mini-svg-data-uri");
 const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -5,10 +6,11 @@ const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenCo
 const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/(card|ripple).js"
   ],
   prefix: "",
   theme: {
@@ -76,22 +78,15 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    addVariablesForColors,
-    function ({ matchUtilities, theme }: any) {
+  plugins: [require(tailwindcss-animate),addVariablesForColors,function ({ matchUtilities,theme }: any) {
       matchUtilities(
         {
-          "bg-dot-thick": (value: any) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+          bg-dot-thick: (value: any) => ({
+            backgroundImage: url(${svgToDataUri(
+              <svg xmlns=http://www.w3.org/2000/svg viewBox=0 0 32 32 width=16 height=16 fill=none><circle fill=${value} id=pattern-circle cx=10 cy=10 r=2.5></circle></svg>
+            )}),}),},{ values: flattenColorPalette(theme(backgroundColor)),type: color }
       );
-    },
-  ],
+    },nextui()],
 };
 
 function addVariablesForColors({ addBase, theme }: any) {
